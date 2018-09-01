cb781042f3d9f852f41a07250f80b790a59b7f9e
xuan <362386736@qq.com>
2018-09-01 10:05:51 +0800

    添加分类图片，修改距离顶部位置

---
+++ js/style.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $("._music").attr("src", "img/music.svg");
+    $("._book").attr("src", "img/book.svg");
+    $("._image").attr("src", "img/image.svg");
+    $("._video").attr("src", "img/video.svg");
+    $("._trip").attr("src", "img/trip.svg");
+    $("._article").attr("src", "img/article.svg");
+
+})
