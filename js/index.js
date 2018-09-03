f1f5fcad158b9cda7e9520c83657408ae214a876
xuan <362386736@qq.com>
2018-09-03 10:29:20 +0800

    修改描述

--- js/index.js
+++ js/index.js
@@ -43,7 +43,6 @@ function initDate() {
         }
         html = html + '</div>';
     }
-    console.log(html);
     $(".content").append(html);
 }
 
--- js/indexList.js
+++ js/indexList.js
@@ -28,7 +28,7 @@ var indexList = {
             imgClass: "_music",
             bgColorClass: "bg-white",
             title: "雨天-戚薇",
-            text: "最讨厌的雨天",
+            text: "雨天不讨厌</br>讨厌的是</br>雨天的离别</br>伤感的画面",
             href: "music.html?id=0",
             time: "2018-07-30 10:06",
         }, {
@@ -37,7 +37,7 @@ var indexList = {
             imgClass: "_music",
             bgColorClass: "bg-orange",
             title: "遇见-张瑶",
-            text: "最讨厌的雨天",
+            text: "遇见</br>是一场意外</br>也许在</br>某个街道</br>某个角落",
             href: "music.html?id=1",
             time: "2018-07-30 10:06",
         }, {
@@ -46,7 +46,7 @@ var indexList = {
             imgClass: "_music",
             bgColorClass: "bg-green",
             title: "爱情雨-张根硕",
-            text: "最讨厌的雨天",
+            text: "一场雨的邂逅</br>伞与雨的碰撞</br>满天星的芳香</br>唯美的Love Story</br>",
             href: "music.html?id=3",
             time: "2018-07-30 10:06",
         }, ]
