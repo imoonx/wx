8fb5f6c28c7ae05f2cfca44d94e8c82a0559e97f
xuan <362386736@qq.com>
2018-09-03 10:10:26 +0800

    修改首页信息，抽取代码

---
+++ js/indexList.js
@@ -0,0 +1,54 @@
+/**
+ * 首页数据列表
+ * 
+ * bgMusic: "music/であい-まつい_ようへい_ミト-201803.mp3", // 首页音乐ID
+ * item: [{//数据item
+ *       id: "1", //数据id
+ *       row: [//行
+ *            {//列
+ *            id:1,//行id
+ *            type:"音乐",//类型描述
+ *            imgClass:"_music",//类型图
+ *            bgColorClass:"bg-orange",//背景色
+ *            title:"爱情雨-张根硕",//标题
+ *            text:"最讨厌的雨天",//显示内容
+ *            href:"music.html?id=3",//跳转路径
+ *            time:"2017-07-30 10:06",时间
+ *      }]
+ *  }, ]
+ * 
+ */
+var indexList = {
+    bgMusic: "music/であい-まつい_ようへい_ミト-201803.mp3",
+    item: [{
+        id: "1",
+        row: [{
+            id: 1,
+            type: "音乐",
+            imgClass: "_music",
+            bgColorClass: "bg-white",
+            title: "雨天-戚薇",
+            text: "最讨厌的雨天",
+            href: "music.html?id=0",
+            time: "2018-07-30 10:06",
+        }, {
+            id: 1,
+            type: "音乐",
+            imgClass: "_music",
+            bgColorClass: "bg-orange",
+            title: "遇见-张瑶",
+            text: "最讨厌的雨天",
+            href: "music.html?id=1",
+            time: "2018-07-30 10:06",
+        }, {
+            id: 1,
+            type: "音乐",
+            imgClass: "_music",
+            bgColorClass: "bg-green",
+            title: "爱情雨-张根硕",
+            text: "最讨厌的雨天",
+            href: "music.html?id=3",
+            time: "2018-07-30 10:06",
+        }, ]
+    }]
+};
